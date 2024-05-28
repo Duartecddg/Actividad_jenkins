66beb7387a26721533ede8a84186cdd67cbd2bb8
Duartecddg <102677809+Duartecddg@users.noreply.github.com>
2024-05-28 00:02:30 -0600

    cambios para correrlo desde el servidor

---
+++ index.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+// Configurar Express para servir archivos estáticos desde la carpeta 'public'
+app.use(express.static('public'));
+
+app.listen(port, () => {
+  console.log(`Servidor escuchando en http://localhost:${port}`);
+});
